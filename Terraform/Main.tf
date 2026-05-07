b11efd05c405f2b5481902a1dc1e08b1d19cdbf0
shivahrr143-max <shivahrr143@gmail.com>
2026-05-07 18:52:19 +0530

    Update Main.tf

--- Terraform/Main.tf
+++ Terraform/Main.tf
@@ -51,8 +51,8 @@ resource "aws_security_group" "web_sg" {
   }
 
   ingress {
-    from_port   = 8081
-    to_port     = 8081
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
